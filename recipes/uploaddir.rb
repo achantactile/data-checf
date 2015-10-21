aa3e7abe0965463ef20254f2d2376eee6abd6242
Markus Gray <m.gray@thetactilegroup.com>
2015-10-21 09:52:00 -0400

    Initial Commit

---
+++ recipes/uploaddir.rb
@@ -0,0 +1,9 @@
+node[:deploy].each do |app_name, deploy|
+	app_root = "#{deploy[:deploy_to]}/current"
+
+	directory "#{app_root}/uploads" do
+		mode: '0777'
+		recursive: true
+		action:create
+	end 
+end
