cd30543974bd1c8a4f9b3fd5bfb55cda5e5cc537
Markus Gray <m.gray@thetactilegroup.com>
2015-10-21 10:11:58 -0400

    Update

--- datacollection/recipes/uploaddir.rb
+++ datacollection/recipes/uploaddir.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |app_name, deploy|
 	app_root = "#{deploy[:deploy_to]}/current"
 
 	directory "#{app_root}/uploads" do
-		mode '0777'
+		mode 0777
 		recursive true
 		action create
 	end 
